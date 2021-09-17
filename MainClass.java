45e5c81045802eafda253a07a93572774b3ce35b
Dhivya Sri Lingala <80615315+DhivyaSriLingala@users.noreply.github.com>
2021-09-17 09:20:40 +0530

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,16 @@
+public class MainClass
+{
+    public static void main(String args[])
+    {
+        subClass sc=new subClass();
+        sc.disp();
+    }
+}
+class subClass
+{
+    String msg="Hello Welcome to the classes";
+    void disp()
+    {
+        System.out.println(msg);
+    }
+}
