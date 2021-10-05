31b4494131deb56e1a0b9bfd3d903bba594bfd38
Dhivya Sri Lingala <80615315+DhivyaSriLingala@users.noreply.github.com>
2021-10-05 10:25:52 +0530

    Create method_overriding.java

---
+++ method_overriding.java
@@ -0,0 +1,34 @@
+class Shapes
+{
+    public void area()
+    {
+        System.out.println("The formula for area of ");
+    }
+}
+class Triangle extends Shapes
+{
+    public void area()
+    {
+        System.out.println("Triangle is ½ * base * height ");
+    }
+}
+class Circle extends Shapes
+{
+    public void area()
+    {
+        System.out.println("Circle is 3.14 * radius * radius ");
+    }
+}
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Shapes myShape = new Shapes();
+        Shapes myTriangle = new Triangle();
+        Shapes myCircle = new Circle();
+        myShape.area();
+        myTriangle.area();
+        myShape.area();
+        myCircle.area();
+    }
+}
